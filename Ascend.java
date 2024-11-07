d0a35a8bd08a8f02bf40fe3e5344b57756ef1b1d
barenzel <bar.enzel@post.runi.ac.il>
2024-11-07 17:19:13 +0200

    completed

--- Ascend.java
+++ Ascend.java
@@ -2,7 +2,7 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		 int lim = Integer.parseInt(args[0]);
+		int lim = Integer.parseInt(args[0]);
 		int a = (int)(Math.random() * lim);
 		int b = (int)(Math.random() * lim);
 		int c = (int)(Math.random() * lim);
