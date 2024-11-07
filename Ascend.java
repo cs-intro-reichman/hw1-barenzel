26ed2ebe97b57f5598ddd9c1b2e1d66f9042d0f4
barenzel <bar.enzel@post.runi.ac.il>
2024-11-07 17:13:57 +0200

    completed

--- Ascend.java
+++ Ascend.java
@@ -3,24 +3,18 @@
 public class Ascend {
 	public static void main(String[] args) {
 		 int lim = Integer.parseInt(args[0]);
-		 // three variables from the range 
-		int a = (int)(Math.random()*lim);
-		int b = (int)(Math.random()*lim);
-		int c = (int)(Math.random()*lim);
- 
+		int a = (int)(Math.random() * lim);
+		int b = (int)(Math.random() * lim);
+		int c = (int)(Math.random() * lim);
 		 // checking the smallest number
-		int checkMin1 = Math.min(a,b);
-		int checkMin2 = Math.min(checkMin1,c);
- 
+		int checkMin1 = Math.min(a, b);
+		int checkMin2 = Math.min(checkMin1, c);
 		// cheking the biggest number
-		int checkMax1 = Math.max(a,b);
-		int checkMax2 = Math.max(checkMax1,c);
- 
+		int checkMax1 = Math.max(a, b);
+		int checkMax2 = Math.max(checkMax1, c);
 		// checking the middle number
-		int middle = (a+b+c)-(checkMax2+checkMin2);
- 
- 
-		System.out.println(a+" "+b+" "+c);
-		System.out.println(checkMin2+" "+middle+" "+checkMax2);
+		int middle = (a + b + c) - (checkMax2 + checkMin2);
+		System.out.println( a + " " + b + " " + c);
+		System.out.println(checkMin2 + " " + middle + " " + checkMax2);
 	}
 }
--- Bill3.java
+++ Bill3.java
@@ -5,9 +5,8 @@ public class Bill3 {
 	    String name1 = args[0];
 	    String name2 = args[1];
         String name3 = args[2];
-        double bill = Double.parseDouble(args[3]);
-            
-          double sum = Math.ceil(bill/3);
-          System.out.println("Dear "+name3+", "+name2+", and "+name1+": pay "+sum+" Shekels each");   
+        double bill = Double.parseDouble(args[3]);    
+        double sum = Math.ceil(bill / 3);
+        System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + sum + " Shekels each");   
 	}
 }
